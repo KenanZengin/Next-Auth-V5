2100984994e327ab0eaffd493bb81e57fa6102d1
fener bahce <xrpchzbtt@gmail.com>
2024-01-07 21:09:30 +0300

    update prisma

--- actions/register.js
+++ actions/register.js
@@ -1,7 +1,10 @@
 "use server" 
 
 import * as z from "zod"
+import bcrypt from "bcrypt"
 import { RegisterSchema } from "@/schema";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values) => { 
     console.log(values);
@@ -11,7 +14,26 @@ export const register = async (values) => {
         return{error:"Invalid fields"}
     }
 
-    return{success: "Email sent"}
+    const {email,password,name} = validateFields.data
+    const hashPassword = await bcrypt.hash(password,10)
+
+    const existingUser = await getUserByEmail(email)
+
+    if(existingUser){
+        return { error : "Eamil aldready in use!"}
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashPassword
+        }
+    })
+
+    //Todo : send verifitication token email
+
+    return{success: "User created"}
 
 
 }
